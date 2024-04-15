f6e03e7e8a28b664456d0b2b43f9a11689573a25
VladFedenkov <woohda@ya.ru>
2024-04-15 19:09:51 +0700

    добавил функционал добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки 
+function addCard(card) {
+    const cardElement = cardTemplate.cloneNode(true);
+    cardElement.querySelector('.card__title').textContent = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__like-button').addEventListener('click', function (event) {
+        event.target.classList.toggle('card__like-button_is-active');
+    });
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+    const cardElement = this.parentElement;
+    cardElement.remove();
+};
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(element => {
+    placesList.append(addCard(element));
+});
